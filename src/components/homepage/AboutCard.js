d9e2e3e92f1916e18fe8f438a6d2b7eac993fcc2
Tram13 <arno.vermote@hotmail.com>
2021-09-26 23:38:37 +0200

    reworked cv button

--- src/components/homepage/AboutCard.js
+++ src/components/homepage/AboutCard.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
 import arno from "../../images/homepage/arno.png"
 import {Link} from "react-router-dom";
+import Table from "react-bootstrap/Table";
 
 class AboutCard extends React.Component {
     render() {
@@ -14,12 +14,23 @@ class AboutCard extends React.Component {
                         I am a Computer Science student at Ghent University. I got my Bachelor's degree in 2021 and now
                         I'm working hard to get my Master's in 2023.
                         <br/>
-                        <br/>
-                        <ButtonGroup style={{paddingLeft: '3rem'}}>
-                            <a className="btn btn-secondary" rel="noopener noreferrer" href="https://github.com/tram13" target="_blank">GitHub</a>
-                            <Link className="btn btn-secondary" to="./cv_vermotearno.pdf" target="_blank" download>Curriculum Vitae</Link>
-                        </ButtonGroup>
                     </Card.Text>
+                    <Table responsive="sm" borderless="true">
+                        <tbody>
+                        <tr>
+                            <td><a className="btn btn-secondary" rel="noopener noreferrer"
+                                   href="https://github.com/tram13"
+                                   target="_blank">GitHub</a></td>
+                            <td/>
+                            <td/>
+                            <td/>
+                            <td/>
+                            <td><Link className="btn btn-secondary" to="/download/cv_vermotearno.pdf" target="_blank"
+                                      download>Curriculum
+                                Vitae</Link></td>
+                        </tr>
+                        </tbody>
+                    </Table>
                 </Card.Body>
                 <Card.Img variant="bottom" src={arno}
                           style={{borderBottomLeftRadius: '20px', borderBottomRightRadius: '20px'}}
